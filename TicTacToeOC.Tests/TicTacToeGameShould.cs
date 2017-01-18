20aeff64c57e7cbeb2f8d9c9378dcbd357e46b0f
Adnan Gazi <adnan.gazi@asos.com>
2017-01-18 15:27:29 +0000

    project setup

--- TicTacToeOC.Tests/TicTacToeGameShould.cs
+++ TicTacToeOC.Tests/TicTacToeGameShould.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TicTacToeOC
+namespace TicTacToeOC.Tests
 {
-    public class Class1
+    public class TicTacToeGameShould
     {
     }
 }
