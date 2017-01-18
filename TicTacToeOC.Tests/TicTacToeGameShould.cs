94ba81bd6ba3b972d3f84e27ce906f7f40d24802
Adnan Gazi <adnan.gazi@asos.com>
2017-01-18 15:52:02 +0000

    empty games are equal

--- TicTacToeOC.Tests/TicTacToeGameShould.cs
+++ TicTacToeOC.Tests/TicTacToeGameShould.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Internal.Execution;
 
 namespace TicTacToeOC.Tests
 {
+    [TestFixture]
     public class TicTacToeGameShould
     {
+        [Test]
+        public void BeEqualToAnotherGame_GivenBothHaveNotBeenPlayed()
+        {
+            var newGame = new TicTacToeGame();
+            var otherNewGame = new TicTacToeGame();
+            newGame.Should().Be(otherNewGame);
+        }
+    }
+
+    public class TicTacToeGame
+    {
+        protected bool Equals(TicTacToeGame other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((TicTacToeGame) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
